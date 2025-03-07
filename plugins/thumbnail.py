4fa11132a4087c87ced69fb76e7f25ce55520892
Sumit-Developer-2-0 <sumitdeveloper2.0@gmail.com>
2025-03-07 13:44:48 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -1,5 +1,6 @@
-from pyrogram import Client, filters, enums  # Added enums import
+from pyrogram import Client, filters 
 from helper.database import jishubotz
+
 
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
 async def viewthumb(client, message):    
@@ -20,17 +21,12 @@ async def addthumbs(client, message):
     await jishubotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await mkn.edit("**Thumbnail Saved Successfully ✅️**")
 
-@Client.on_message(filters.command('id'))
-async def show_id(client, message):
-    chat_type = message.chat.type
-    if chat_type == enums.ChatType.PRIVATE:
-        await message.reply_text(f"<b>» ᴜꜱᴇʀ ɪᴅ - <code>{message.from_user.id}</code></b>")
 
-    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        await message.reply_text(f"<b>» ɢʀᴏᴜᴘ ɪᴅ - <code>{message.chat.id}</code></b>")
 
-    elif chat_type == enums.ChatType.CHANNEL:
-        await message.reply_text(f"<b>» ᴄʜᴀɴɴᴇʟ ɪᴅ - <code>{message.chat.id}</code></b>")
+
+
+
+
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
